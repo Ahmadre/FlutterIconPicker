e31ea8547597bc1f557dd290e89870390083e2b5
Rebar Ahmad <rebar.ahmad@gmail.com>
2021-02-20 19:22:21 +0100

    Update widget_test.dart

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -10,20 +10,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fluttericonpickerexample/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(HomeScreen());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  testWidgets('example', (WidgetTester tester) async {});
 }
