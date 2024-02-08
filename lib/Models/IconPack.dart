d5291cd19464860f6785f9faeda404f93c4262b4
Rebar Ahmad <rebar.ahmad@gmail.com>
2024-02-08 23:21:24 +0100

    Separate Material Icon Packs

--- lib/Models/IconPack.dart
+++ lib/Models/IconPack.dart
@@ -1,7 +1,19 @@
 /// The IconPack defines which Icons are gonna be loaded
 enum IconPack {
-  /// The official Material Icons by Flutter
+  /// The official Material Icons by Flutter (without rounded, outlined or sharp icons)
   material,
+
+  /// The official Material Icons by Flutter (including rounded, outlined or sharp icons)
+  allMaterial,
+
+  /// The official Material Sharp Icons by Flutter
+  sharpMaterial,
+
+  /// The official Material Rounded Icons by Flutter
+  roundedMaterial,
+
+  /// The official Material Outlined Icons by Flutter
+  outlinedMaterial,
 
   /// The official Cupertino Icons (Apple Design)
   cupertino,
