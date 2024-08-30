28282dfb4da9b50bb4d567f790745575ab222a16
Rebar Ahmad <rebar.ahmad@gmail.com>
2024-08-30 02:17:07 +0200

    Migrate initial packs

--- lib/IconPicker/Packs/Cupertino.dart
+++ lib/IconPicker/Packs/Cupertino.dart
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter_iconpicker/Models/icon_picker_icon.dart';
 
 const String iconFont = 'CupertinoIcons';
 const String iconFontPackage = 'cupertino_icons';
 
-const Map<String, IconData> cupertinoIcons = {};
+const Map<String, IconPickerIcon> cupertinoIcons = {};
