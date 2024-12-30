501b1dcc8207baacf3cbf2dd83fe0ce7bdac63d6
Rebar Ahmad <rebar.ahmad@gmail.com>
2024-12-30 22:06:45 +0100

    Migrate to data

--- lib/IconPicker/icon_picker.dart
+++ lib/IconPicker/icon_picker.dart
@@ -49,8 +49,7 @@ class FIPIconPicker extends StatefulWidget {
 
 class _FIPIconPickerState extends State<FIPIconPicker> {
   ScrollController scrollController = ScrollController();
-  late GridObserverController observerController =
-      GridObserverController(controller: scrollController);
+  late GridObserverController observerController = GridObserverController(controller: scrollController);
 
   bool scrolledToSelectedIcon = false;
   int selectedIconIndex = -1;
@@ -67,23 +66,22 @@ class _FIPIconPickerState extends State<FIPIconPicker> {
       if (widget.iconPack != null) {
         for (var pack in widget.iconPack!) {
           if (mounted) {
-            widget.iconController.addAll(IconPackManager.getIcons(pack));
+            widget.iconController.addAll(pack.data ?? {});
           }
         }
         if (mounted &&
             !scrolledToSelectedIcon &&
             widget.iconController.shouldScrollToSelectedIcon &&
             widget.iconController.isSelectedIconAvailable &&
-            widget.iconController.entries.firstWhereOrNull((item) =>
-                    widget.iconController.selectedIcon == item.value) !=
+            widget.iconController.entries
+                    .firstWhereOrNull((item) => widget.iconController.selectedIcon == item.value) !=
                 null) {
           Timer.periodic(const Duration(milliseconds: 100), (timer) async {
             if (scrollController.hasClients) {
               scrolledToSelectedIcon = true;
               selectedIconIndex = widget.iconController.entries
                   .toList()
-                  .indexWhere((item) =>
-                      widget.iconController.selectedIcon == item.value);
+                  .indexWhere((item) => widget.iconController.selectedIcon == item.value);
               setState(() {});
               await observerController.jumpTo(
                 index: selectedIconIndex,
@@ -105,22 +103,14 @@ class _FIPIconPickerState extends State<FIPIconPicker> {
             text: TextSpan(
               text: '${widget.noResultsText!} ',
               style: TextStyle(
-                color: widget.backgroundColor != null
-                    ? FIPColorBrightness(widget.backgroundColor!).isLight()
-                        ? Colors.black
-                        : Colors.white
-                    : null,
+                color: FIPColorBrightness(widget.backgroundColor!).isLight() ? Colors.black : Colors.white,
               ),
               children: [
                 TextSpan(
                   text: widget.iconController.searchTextController.text,
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    color: widget.backgroundColor != null
-                        ? FIPColorBrightness(widget.backgroundColor!).isLight()
-                            ? Colors.black
-                            : Colors.white
-                        : null,
+                    color: FIPColorBrightness(widget.backgroundColor!).isLight() ? Colors.black : Colors.white,
                   ),
                 ),
               ],
@@ -149,16 +139,12 @@ class _FIPIconPickerState extends State<FIPIconPicker> {
                         childAspectRatio: 1 / 1,
                         mainAxisSpacing: widget.mainAxisSpacing ?? 5,
                         crossAxisSpacing: widget.crossAxisSpacing ?? 5,
-                        maxCrossAxisExtent: widget.iconSize != null
-                            ? widget.iconSize! + 10
-                            : 50,
+                        maxCrossAxisExtent: widget.iconSize != null ? widget.iconSize! + 10 : 50,
                       ),
                       itemBuilder: (context, index) {
-                        final MapEntry<String, IconPickerIcon> item =
-                            controller.entries.elementAt(index);
+                        final MapEntry<String, IconPickerIcon> item = controller.entries.elementAt(index);
 
-                        final isSelected = widget
-                                .iconController.isSelectedIconAvailable &&
+                        final isSelected = widget.iconController.isSelectedIconAvailable &&
                             widget.iconController.selectedIcon! == item.value;
 
                         if (controller.iconBuilder != null) {
@@ -170,11 +156,8 @@ class _FIPIconPickerState extends State<FIPIconPicker> {
                           );
                         }
 
-                        final selectedIconColor =
-                            widget.selectedIconBackgroundColor ??
-                                (Theme.of(context).brightness == Brightness.dark
-                                    ? Colors.grey[800]
-                                    : Colors.grey[400]);
+                        final selectedIconColor = widget.selectedIconBackgroundColor ??
+                            (Theme.of(context).brightness == Brightness.dark ? Colors.grey[800] : Colors.grey[400]);
 
                         return ClipRRect(
                           borderRadius: BorderRadius.circular(8),
@@ -183,8 +166,7 @@ class _FIPIconPickerState extends State<FIPIconPicker> {
                             child: GestureDetector(
                               onTap: () => controller.onTapIcon(
                                 item.value,
-                                onSelected: () =>
-                                    Navigator.pop(context, item.value),
+                                onSelected: () => Navigator.pop(context, item.value),
                               ),
                               child: widget.showTooltips!
                                   ? Tooltip(
@@ -211,8 +193,7 @@ class _FIPIconPickerState extends State<FIPIconPicker> {
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                       begin: Alignment.topCenter,
-                      end: Alignment.lerp(
-                          Alignment.topCenter, Alignment.center, .05)!,
+                      end: Alignment.lerp(Alignment.topCenter, Alignment.center, .05)!,
                       colors: [
                         widget.backgroundColor ??
                             Theme.of(context).dialogTheme.backgroundColor ??
@@ -235,8 +216,7 @@ class _FIPIconPickerState extends State<FIPIconPicker> {
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                       begin: Alignment.bottomCenter,
-                      end: Alignment.lerp(
-                          Alignment.bottomCenter, Alignment.center, .05)!,
+                      end: Alignment.lerp(Alignment.bottomCenter, Alignment.center, .05)!,
                       colors: [
                         widget.backgroundColor ??
                             Theme.of(context).dialogTheme.backgroundColor ??
--- lib/IconPicker/search_bar.dart
+++ lib/IconPicker/search_bar.dart
@@ -7,8 +7,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_iconpicker/Helpers/icon_pack_manager.dart';
 import 'package:flutter_iconpicker/Models/icon_picker_icon.dart';
 import 'package:flutter_iconpicker/controllers/icon_controller.dart';
+import 'package:flutter_iconpicker/extensions/map_extensions.dart';
 import 'package:provider/provider.dart';
 import '../Models/icon_pack.dart';
+import '../Helpers/color_brightness.dart';
 
 class FIPSearchBar extends StatefulWidget {
   const FIPSearchBar({
@@ -38,36 +40,26 @@ class FIPSearchBar extends StatefulWidget {
 
 class _FIPSearchBarState extends State<FIPSearchBar> {
   final SearchComparator _defaultSearchComparator =
-      (String searchValue, IconPickerIcon icon) =>
-          icon.name.toLowerCase().contains(searchValue.toLowerCase());
-  late final searchComparator =
-      widget.searchComparator ?? _defaultSearchComparator;
+      (String searchValue, IconPickerIcon icon) => icon.name.toLowerCase().contains(searchValue.toLowerCase());
+  late final searchComparator = widget.searchComparator ?? _defaultSearchComparator;
 
   void _search(String searchValue) {
     List<MapEntry<String, IconPickerIcon>> searchResult = [];
 
-    final Iterable<MapEntry<String, IconPickerIcon>> flatFIPPacks = widget
-            .iconPack
-            ?.expand((pack) => IconPackManager.getIcons(pack).entries) ??
-        [];
+    final flatFIPPacks =
+        widget.iconPack?.where((p) => p.data.isNotNullOrEmpty).expand((pack) => pack.data!.entries) ?? [];
     final flatCustomPacks = widget.customIconPack?.entries ?? [];
 
     for (MapEntry<String, IconPickerIcon> item in flatFIPPacks) {
       if (searchComparator.call(
-          searchValue,
-          IconPickerIcon(
-              name: item.key, data: item.value.data, pack: item.value.pack))) {
+          searchValue, IconPickerIcon(name: item.key, data: item.value.data, pack: item.value.pack))) {
         searchResult.add(item);
       }
     }
 
     for (var item in flatCustomPacks) {
       if (searchComparator.call(
-          searchValue,
-          IconPickerIcon(
-              name: item.key,
-              data: item.value.data,
-              pack: IconPack.custom.name))) {
+          searchValue, IconPickerIcon(name: item.key, data: item.value.data, pack: IconPack.custom))) {
         searchResult.add(item);
       }
     }
@@ -87,8 +79,14 @@ class _FIPSearchBarState extends State<FIPSearchBar> {
       return TextField(
         onChanged: (val) => _search(val),
         controller: controller.searchTextController,
+        style: TextStyle(
+          color: FIPColorBrightness(widget.backgroundColor!).isLight() ? Colors.black : Colors.white,
+        ),
         decoration: InputDecoration(
           contentPadding: const EdgeInsets.only(top: 15),
+          hintStyle: TextStyle(
+            color: FIPColorBrightness(widget.backgroundColor!).isLight() ? Colors.black54 : Colors.white54,
+          ),
           hintText: widget.searchHintText,
           prefixIcon: widget.searchIcon,
           suffixIcon: AnimatedSwitcher(
@@ -104,7 +102,7 @@ class _FIPSearchBarState extends State<FIPSearchBar> {
 
                       if (widget.iconPack != null) {
                         for (var pack in widget.iconPack!) {
-                          controller.addAll(IconPackManager.getIcons(pack));
+                          controller.addAll(pack.data ?? {});
                         }
                       }
                     }),
