b773f04a730aa13c23b2afb622a0074a0c3bbdb2
Rebar Ahmad <rebar.ahmad@gmail.com>
2024-12-30 22:08:16 +0100

    Update IconPack enum

--- lib/Models/icon_pack.dart
+++ lib/Models/icon_pack.dart
@@ -1,4 +1,14 @@
-import 'package:collection/collection.dart';
+import '../IconPicker/Packs/Cupertino.dart';
+import '../IconPicker/Packs/EvaIconsFill.dart';
+import '../IconPicker/Packs/EvaIconsOutline.dart';
+import '../IconPicker/Packs/Material.dart';
+import '../IconPicker/Packs/MaterialDefault.dart';
+import '../IconPicker/Packs/MaterialOutlined.dart';
+import '../IconPicker/Packs/MaterialRounded.dart';
+import '../IconPicker/Packs/MaterialSharp.dart';
+import '../IconPicker/Packs/FontAwesome.dart' as FA;
+import '../IconPicker/Packs/LineIcons.dart' as LI;
+import 'icon_picker_icon.dart';
 
 /// The IconPack defines which Icons are gonna be loaded
 enum IconPack {
@@ -6,67 +16,82 @@ enum IconPack {
   material(
     path: 'MaterialDefault',
     description: 'Material Icons by Flutter',
+    data: defaultIcons,
   ),
 
   /// The official Material Icons by Flutter (including rounded, outlined or sharp icons)
   allMaterial(
     path: 'Material',
     description: 'All Material Icons (including rounded, outlined or sharp icons)',
+    data: allIcons,
   ),
 
   /// The official Material Sharp Icons by Flutter
   sharpMaterial(
     path: 'MaterialSharp',
     description: 'Material Sharp Icons',
+    data: sharpIcons,
   ),
 
   /// The official Material Rounded Icons by Flutter
   roundedMaterial(
     path: 'MaterialRounded',
     description: 'Material Rounded Icons',
+    data: roundedIcons,
   ),
 
   /// The official Material Outlined Icons by Flutter
   outlinedMaterial(
     path: 'MaterialOutlined',
     description: 'Material Outlined Icons',
+    data: outlinedIcons,
   ),
 
   /// The official Cupertino Icons (Apple Design)
   cupertino(
     path: 'Cupertino',
     description: 'Cupertino Icons (Apple Design)',
+    data: cupertinoIcons,
   ),
 
   /// The official font_awesome_flutter Icons by the Flutter Community (Brian Egan)
   fontAwesomeIcons(
     path: 'FontAwesome',
     description: 'Font Awesome Icons',
+    data: FA.fontAwesomeIcons,
   ),
 
   /// The official line_awesome_icons Icons by Phuc Chau
   lineAwesomeIcons(
     path: 'LineIcons',
     description: 'Line Awesome Icons',
+    data: LI.lineAwesomeIcons,
   ),
 
   /// The official Eva Icons Fill by Akveo
-  evaIconsFill(
+  evaFilled(
     path: 'EvaIconsFill',
     description: 'Eva Icons Filled',
+    data: evaIconsFilled,
   ),
 
   /// The official Eva Icons Outline by Akveo
-  evaIconsOutline(
+  evaOutlined(
     path: 'EvaIconsOutline',
     description: 'Eva Icons Outlined',
+    data: evaIconsOutlined,
   ),
 
   /// Use this to show your own custom provided IconPack
   custom;
 
-  const IconPack({this.path, this.description});
+  const IconPack({
+    this.path,
+    this.description,
+    this.data,
+  });
 
   final String? path;
   final String? description;
+  final Map<String, IconPickerIcon>? data;
 }
